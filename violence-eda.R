503bb71397fb5ba54478519fdd57e886883dba27
arnagaraja <arnagaraja@gmail.com>
2018-05-16 09:29:14 -0500

    More additions.

--- violence-eda.R
+++ violence-eda.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 library(RColorBrewer)
+library(ggmap)
 
 df <- read_csv("data/gun-violence-data_01-2013_03-2018.csv")
 
@@ -51,5 +52,63 @@ no_axes <- theme_bw() + theme(
       panel.grid = element_blank(),
       axis.title = element_blank()
 )
-p1 <- ggplot(data = statemap, aes(x = long, y = lat, group = group)) + geom_polygon(fill = "white", color = "black") + coord_fixed(1.3) + no_axes
-p2 <- p1 + geom_point(data = gun, aes(x = longitude, y = latitude, col = n_killed, size = 0.001, alpha = .1))
+p1 <- ggplot(data = statemap, aes(x = long, y = lat, group = group)) + geom_polygon(fill = "white", color = "black") + coord_fixed(1.3, xlim = c(-130,-60), ylim = c(20,50))
+p2 <- p1 + geom_point(data = gun, aes(x = longitude, y = latitude, col = n_killed), size = 0.001, alpha = .1, inherit.aes = FALSE) + theme_void() + theme(legend.position = "none")
+#p2
+
+png("p2.png", width = 2056, height = 2056)
+p2
+dev.off()
+
+
+# READ IN US POPULATION DATA BY STATE
+colNames <- c("id1", "id2", "state", "census2010", "cenbase2010", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017")
+pop <- read_csv("data/US\ Population\ by\ State/PEP_2017_PEPANNRES_with_ann.csv", skip = 2, col_names = colNames)
+
+# Only keep the columns and rows we want
+pop <- pop %>% select(id1, state, "2014", "2015", "2016", "2017") %>% slice(6:n())
+
+# Casualties by state and year; remove Washington DC from the state rankings, since it will skew the results
+casByState <- gun %>% group_by(state, year = format(date, "%Y")) %>% filter(state != "District of Columbia") %>% 
+      summarize(n.casualty = sum(n_killed + n_injured))
+
+
+
+# Add the population and the per capita rate (per 100K people)
+
+# First, generate the population vector:
+popList <- numeric()
+for (i in 1:nrow(casByState)) {
+      popList <- c(popList, as.numeric(pop[pop$state == casByState$state[i], casByState$year[i]]))
+}
+
+casByState <- as.data.frame(casByState) %>% mutate(population = popList, per.capita100K = n.casualty/population*100000) %>% as_tibble()
+
+# p1: Casualty rate for all years for all states; hard to determine a pattern
+g <- ggplot(data = casByState, aes(x = reorder(state, -per.capita100K), y = per.capita100K, fill = year))
+p1 <- g + geom_col(position = position_dodge()) + 
+      theme(axis.text.x  = element_text(angle=90, vjust=0.5, size = 14)) + 
+      theme(axis.title.y = element_text(size = 16)) + 
+      theme(plot.title = element_text(size = 16)) + 
+      labs(x = NULL, y = "Casualty Rate per 100,000 People", 
+           title = "Casualty Rate by State per 100,000 People (2014-2017)")
+
+
+# Just plot the mean for each year
+casByStateMean <- group_by(casByState, state) %>% summarize(meanCasRate = mean(per.capita100K))
+
+g <- ggplot(data = casByStateMean, aes(x = reorder(state, -meanCasRate), y = meanCasRate))
+p2 <- g + geom_col(position = position_dodge(), fill = cols[2]) + 
+      theme(axis.text.x  = element_text(angle=90, vjust=0.5, size = 14)) + 
+      theme(axis.title.y = element_text(size = 16)) + 
+      theme(plot.title = element_text(size = 16)) + 
+      labs(x = NULL, y = "Mean Casualty Rate per 100,000 People", 
+           title = "Mean Casualty Rate per 100,000 People by State (2014-2017)")
+
+# Add a line for the median
+p3 <- p2 + geom_hline(yintercept = median(casByStateMean$meanCasRate), lwd = 1)
+
+# To Do:
+# Use plotly to show value when hovering over the bar
+# Classify each state based on strictness of gun laws
+# Look at cities
